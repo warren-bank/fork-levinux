21c2cb85e3332e2b063b2e1e5147e29c74a37f8d
Warren R Bank <warren.r.bank@gmail.com>
2023-07-10 17:00:35 -0700

    separate 'hook_boot' script into 2 parts: synchronous and asynchronous
    any command that produces output should be called from the sync script

--- tftp/Recipe.sh
+++ tftp/Recipe.sh
@@ -28,6 +28,7 @@ configure_user() {
   echo 'etc/shadow' >> '/opt/.filetool.lst'
 }
 
+# [async] run script from: 'bootlocal.sh'
 install_extension_dropbear() {
   tftp -g -l "${guest_dir_tcz}/dropbear.tcz" -r "${tftp_dir_extensions}/dropbear/dropbear.tcz" 10.0.2.2
 
@@ -43,6 +44,7 @@ install_extension_dropbear() {
   echo 'usr/local/etc/dropbear/dropbear_rsa_host_key'    >> '/opt/.filetool.lst'
 }
 
+# [async] run script from: 'bootlocal.sh'
 install_extension_busybox_httpd() {
   tftp -g -l "${guest_dir_tcz}/busybox-httpd.tcz" -r "${tftp_dir_extensions}/busybox-httpd/busybox-httpd.tcz" 10.0.2.2
 
@@ -79,20 +81,46 @@ prepare_installer_scripts() {
   prepare_installer_script 'install_vim.sh'
 }
 
+# [sync] run script from: 'bootsync.sh'
 prepare_custom_extras() {
   [ -d "$guest_dir_extras" ] || mkdir "$guest_dir_extras"
   tftp -g -l "${guest_dir_extras}/install_extras.sh" -r "${tftp_dir_foundation}/install_extras.sh" 10.0.2.2
   prepare_bash_script "${guest_dir_extras}/install_extras.sh"
 
-  echo "'${guest_dir_extras}/install_extras.sh' '${guest_dir_extras}/extras.lst' '${guest_dir_tcz}' '${tftp_dir_customize}/extras.lst' '${tftp_dir_extras}' > /dev/null" >> '/opt/bootlocal.sh'
+  echo "'${guest_dir_extras}/install_extras.sh' '${guest_dir_extras}/extras.lst' '${guest_dir_tcz}' '${tftp_dir_customize}/extras.lst' '${tftp_dir_extras}' > /dev/null" >> '/opt/bootsync.sh'
 }
 
-prepare_boot_hook() {
-  tftp -g -l '/etc/hook_boot.sh' -r "${tftp_dir_foundation}/hook_boot.sh" 10.0.2.2
-  prepare_bash_script '/etc/hook_boot.sh'
+# invoke directly because 'bootsync.sh' is already running
+run_custom_extras() {
+  "${guest_dir_extras}/install_extras.sh" "${guest_dir_extras}/extras.lst" "$guest_dir_tcz" "${tftp_dir_customize}/extras.lst" "$tftp_dir_extras"
+}
 
-  echo '/etc/hook_boot.sh' >> '/opt/bootlocal.sh'
-  echo 'etc/hook_boot.sh'  >> '/opt/.filetool.lst'
+# [async] run script from: 'bootlocal.sh'
+prepare_boot_hook_async() {
+  tftp -g -l '/etc/hook_boot_async.sh' -r "${tftp_dir_foundation}/hook_boot_async.sh" 10.0.2.2
+  prepare_bash_script '/etc/hook_boot_async.sh'
+
+  echo '/etc/hook_boot_async.sh' >> '/opt/bootlocal.sh'
+  echo 'etc/hook_boot_async.sh'  >> '/opt/.filetool.lst'
+}
+
+# [sync] run script from: 'bootsync.sh'
+prepare_boot_hook_sync() {
+  tftp -g -l '/etc/hook_boot_sync.sh' -r "${tftp_dir_foundation}/hook_boot_sync.sh" 10.0.2.2
+  prepare_bash_script '/etc/hook_boot_sync.sh'
+
+  echo '/etc/hook_boot_sync.sh' >> '/opt/bootsync.sh'
+  echo 'etc/hook_boot_sync.sh'  >> '/opt/.filetool.lst'
+}
+
+# invoke directly because 'bootsync.sh' is already running
+run_boot_hook_sync() {
+  '/etc/hook_boot_sync.sh'
+}
+
+prepare_boot_hooks() {
+  prepare_boot_hook_async
+  prepare_boot_hook_sync
 }
 
 prepare_login_hook() {
@@ -110,10 +138,13 @@ prepare_recipe() {
   install_extension_busybox_httpd
   prepare_installer_scripts
   prepare_custom_extras
-  prepare_boot_hook
+  prepare_boot_hooks
   prepare_login_hook
 
   filetool.sh -b
+
+  run_custom_extras
+  run_boot_hook_sync
 }
 
 prepare_recipe
